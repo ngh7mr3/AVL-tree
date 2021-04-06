af74e34fd99a8c780d92b215f8a098d0773c3637
ngh7mr3 <20752642+ngh7mr3@users.noreply.github.com>
2021-04-06 12:31:42 +0300

    Created base for binary tree

---
+++ tree.py
@@ -0,0 +1,55 @@
+class Node:
+    __slots__ = '__data', 'parent', 'left', 'right'
+
+    def __init__(self, data):
+        self.__data = data
+        self.parent = None
+        self.left = None
+        self.right = None
+
+    @property
+    def data(self):
+        return self.__data
+
+    def __lt__(self, node):
+        return self.data < node.data
+
+    def __gt__(self, node):
+        return self.data > node.data
+
+    def __repr__(self):
+        return f"(value: {self.data}, left: {self.left}, right: {self.right})"
+
+
+class BinaryTree:
+    def __init__(self, nodes=[]):
+        self.__root = None
+        for node in nodes:
+            self.add(node)
+
+    @property
+    def root(self):
+        return self.__root
+
+    def add(self, key):
+        new_node = Node(key)
+        if not self.__root:
+            self.__root = new_node
+            return
+
+        current_node = self.__root
+        while current_node != None:
+            new_node.parent = current_node
+            if current_node > new_node:
+                current_node = current_node.left
+            else:
+                current_node = current_node.right
+
+        if new_node.parent > new_node:
+            new_node.parent.left = new_node
+        else:
+            new_node.parent.right = new_node
+
+    def find(self, key):
+        pass
+
